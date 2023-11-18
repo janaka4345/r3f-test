1c23aac7ae96b7c851d5525ccedd22f513f761f2
J_ Keyy <ashnazg1212@gmail.com>
2023-11-18 11:25:47 +0000

    Update 9 files

--- app/shader/ShaderExperience.jsx
+++ app/shader/ShaderExperience.jsx
@@ -1,6 +1,8 @@
 // import { Suspense } from "react";
 // import Plane from "../../3d/Plane";
 import PlaneShader from "./PlaneShader";
+
+import { Sparkles, Center } from "@react-three/drei";
 // import Placeholder from "../../3d/Placeholder";
 export default function ShaderExperience() {
   return (
@@ -8,7 +10,10 @@ export default function ShaderExperience() {
       <ambientLight intensity={0.5} />
       <directionalLight position={[1, 2, 3]} intensity={1.5} />
       {/* <Plane /> */}
-      <PlaneShader />
+      <Center>
+        <PlaneShader />
+        <Sparkles size={6} speed={0.2} />
+      </Center>
     </>
   );
 }
